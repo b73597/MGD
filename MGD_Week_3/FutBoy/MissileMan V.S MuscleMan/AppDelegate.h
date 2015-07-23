7b7f9b08a15b272e0bb40a61133737674b006ef3
EdPiedra <davilamschool@gmail.com>
2015-07-23 13:20:08 -0500

    MGD Week 3 Assignment
    Diff size settings, pause, sprite animo, win/lose state, score

---
+++ MGD_Week_3/FutBoy/MissileMan V.S MuscleMan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FutBoy
+//
+//  Created by Omar Davila on 7/6/15.
+//  Copyright (c) 2015 Omar Davila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MGD_Week_3/FutBoy/MissileMan V.S MuscleMan/FutBoyChar.h
@@ -0,0 +1,121 @@
+// ---------------------------------------
+// Sprite definitions for character
+// Generated with TexturePacker 3.9.1
+//
+// http://www.codeandweb.com/texturepacker
+// ---------------------------------------
+
+#ifndef __CHARACTER_ATLAS__
+#define __CHARACTER_ATLAS__
+
+// ------------------------
+// name of the atlas bundle
+// ------------------------
+#define CHARACTER_ATLAS_NAME @"character"
+
+// ------------
+// sprite names
+// ------------
+#define CHARACTER_SPR_BACKGROUND                         @"background"
+#define CHARACTER_SPR_BALLANIMATION_BALL_0               @"BallAnimation/ball_0"
+#define CHARACTER_SPR_BALLANIMATION_BALL_1               @"BallAnimation/ball_1"
+#define CHARACTER_SPR_BALLANIMATION_BALL_2               @"BallAnimation/ball_2"
+#define CHARACTER_SPR_BALLANIMATION_BALL_3               @"BallAnimation/ball_3"
+#define CHARACTER_SPR_BUTTONS_CONTROLS_SHOOTBUTTONSPRITE @"Buttons&Controls/ShootButtonSprite"
+#define CHARACTER_SPR_CRYANIMATION_CRY_0                 @"CryAnimation/cry_0"
+#define CHARACTER_SPR_CRYANIMATION_CRY_1                 @"CryAnimation/cry_1"
+#define CHARACTER_SPR_CRYANIMATION_CRY_2                 @"CryAnimation/cry_2"
+#define CHARACTER_SPR_DIRARROW_DOWN_ARROW                @"DirArrow/down_arrow"
+#define CHARACTER_SPR_DIRARROW_LEFT_ARROW                @"DirArrow/left_arrow"
+#define CHARACTER_SPR_DIRARROW_RIGHT_ARROW               @"DirArrow/right_arrow"
+#define CHARACTER_SPR_DIRARROW_UP_ARROW                  @"DirArrow/up_arrow"
+#define CHARACTER_SPR_FEEDBACK_CHEER_CROWD               @"Feedback/cheer_crowd"
+#define CHARACTER_SPR_FEEDBACK_SCORE_BK                  @"Feedback/score_bk"
+#define CHARACTER_SPR_FEEDBACK_STAGE_CLEAR               @"Feedback/stage_clear"
+#define CHARACTER_SPR_RUNANIMATION_RUN_0                 @"RunAnimation/run_0"
+#define CHARACTER_SPR_RUNANIMATION_RUN_1                 @"RunAnimation/run_1"
+#define CHARACTER_SPR_RUNANIMATION_RUN_2                 @"RunAnimation/run_2"
+#define CHARACTER_SPR_RUNANIMATION_RUN_3                 @"RunAnimation/run_3"
+#define CHARACTER_SPR_RUNANIMATION_RUN_4                 @"RunAnimation/run_4"
+#define CHARACTER_SPR_RUNANIMATION_RUN_5                 @"RunAnimation/run_5"
+#define CHARACTER_SPR_RUNANIMATION_RUN_6                 @"RunAnimation/run_6"
+#define CHARACTER_SPR_RUNANIMATION_RUN_7                 @"RunAnimation/run_7"
+#define CHARACTER_SPR_SHOOTANIMATION_SHOOT_0             @"ShootAnimation/shoot_0"
+#define CHARACTER_SPR_SHOOTANIMATION_SHOOT_1             @"ShootAnimation/shoot_1"
+#define CHARACTER_SPR_SHOOTANIMATION_SHOOT_2             @"ShootAnimation/shoot_2"
+#define CHARACTER_SPR_SHOOTANIMATION_SHOOT_3             @"ShootAnimation/shoot_3"
+#define CHARACTER_SPR_SHOOTANIMATION_SHOOT_4             @"ShootAnimation/shoot_4"
+#define CHARACTER_SPR_SHOOTANIMATION_SHOOT_5             @"ShootAnimation/shoot_5"
+
+// --------
+// textures
+// --------
+#define CHARACTER_TEX_BACKGROUND                         [SKTexture textureWithImageNamed:@"background"]
+#define CHARACTER_TEX_BALLANIMATION_BALL_0               [SKTexture textureWithImageNamed:@"BallAnimation/ball_0"]
+#define CHARACTER_TEX_BALLANIMATION_BALL_1               [SKTexture textureWithImageNamed:@"BallAnimation/ball_1"]
+#define CHARACTER_TEX_BALLANIMATION_BALL_2               [SKTexture textureWithImageNamed:@"BallAnimation/ball_2"]
+#define CHARACTER_TEX_BALLANIMATION_BALL_3               [SKTexture textureWithImageNamed:@"BallAnimation/ball_3"]
+#define CHARACTER_TEX_BUTTONS_CONTROLS_SHOOTBUTTONSPRITE [SKTexture textureWithImageNamed:@"Buttons&Controls/ShootButtonSprite"]
+#define CHARACTER_TEX_CRYANIMATION_CRY_0                 [SKTexture textureWithImageNamed:@"CryAnimation/cry_0"]
+#define CHARACTER_TEX_CRYANIMATION_CRY_1                 [SKTexture textureWithImageNamed:@"CryAnimation/cry_1"]
+#define CHARACTER_TEX_CRYANIMATION_CRY_2                 [SKTexture textureWithImageNamed:@"CryAnimation/cry_2"]
+#define CHARACTER_TEX_DIRARROW_DOWN_ARROW                [SKTexture textureWithImageNamed:@"DirArrow/down_arrow"]
+#define CHARACTER_TEX_DIRARROW_LEFT_ARROW                [SKTexture textureWithImageNamed:@"DirArrow/left_arrow"]
+#define CHARACTER_TEX_DIRARROW_RIGHT_ARROW               [SKTexture textureWithImageNamed:@"DirArrow/right_arrow"]
+#define CHARACTER_TEX_DIRARROW_UP_ARROW                  [SKTexture textureWithImageNamed:@"DirArrow/up_arrow"]
+#define CHARACTER_TEX_FEEDBACK_CHEER_CROWD               [SKTexture textureWithImageNamed:@"Feedback/cheer_crowd"]
+#define CHARACTER_TEX_FEEDBACK_SCORE_BK                  [SKTexture textureWithImageNamed:@"Feedback/score_bk"]
+#define CHARACTER_TEX_FEEDBACK_STAGE_CLEAR               [SKTexture textureWithImageNamed:@"Feedback/stage_clear"]
+#define CHARACTER_TEX_RUNANIMATION_RUN_0                 [SKTexture textureWithImageNamed:@"RunAnimation/run_0"]
+#define CHARACTER_TEX_RUNANIMATION_RUN_1                 [SKTexture textureWithImageNamed:@"RunAnimation/run_1"]
+#define CHARACTER_TEX_RUNANIMATION_RUN_2                 [SKTexture textureWithImageNamed:@"RunAnimation/run_2"]
+#define CHARACTER_TEX_RUNANIMATION_RUN_3                 [SKTexture textureWithImageNamed:@"RunAnimation/run_3"]
+#define CHARACTER_TEX_RUNANIMATION_RUN_4                 [SKTexture textureWithImageNamed:@"RunAnimation/run_4"]
+#define CHARACTER_TEX_RUNANIMATION_RUN_5                 [SKTexture textureWithImageNamed:@"RunAnimation/run_5"]
+#define CHARACTER_TEX_RUNANIMATION_RUN_6                 [SKTexture textureWithImageNamed:@"RunAnimation/run_6"]
+#define CHARACTER_TEX_RUNANIMATION_RUN_7                 [SKTexture textureWithImageNamed:@"RunAnimation/run_7"]
+#define CHARACTER_TEX_SHOOTANIMATION_SHOOT_0             [SKTexture textureWithImageNamed:@"ShootAnimation/shoot_0"]
+#define CHARACTER_TEX_SHOOTANIMATION_SHOOT_1             [SKTexture textureWithImageNamed:@"ShootAnimation/shoot_1"]
+#define CHARACTER_TEX_SHOOTANIMATION_SHOOT_2             [SKTexture textureWithImageNamed:@"ShootAnimation/shoot_2"]
+#define CHARACTER_TEX_SHOOTANIMATION_SHOOT_3             [SKTexture textureWithImageNamed:@"ShootAnimation/shoot_3"]
+#define CHARACTER_TEX_SHOOTANIMATION_SHOOT_4             [SKTexture textureWithImageNamed:@"ShootAnimation/shoot_4"]
+#define CHARACTER_TEX_SHOOTANIMATION_SHOOT_5             [SKTexture textureWithImageNamed:@"ShootAnimation/shoot_5"]
+
+// ----------
+// animations
+// ----------
+#define CHARACTER_ANIM_BALLANIMATION_BALL @[ \
+        [SKTexture textureWithImageNamed:@"BallAnimation/ball_0"], \
+        [SKTexture textureWithImageNamed:@"BallAnimation/ball_1"], \
+        [SKTexture textureWithImageNamed:@"BallAnimation/ball_2"], \
+        [SKTexture textureWithImageNamed:@"BallAnimation/ball_3"]  \
+    ]
+
+#define CHARACTER_ANIM_CRYANIMATION_CRY @[ \
+        [SKTexture textureWithImageNamed:@"CryAnimation/cry_0"], \
+        [SKTexture textureWithImageNamed:@"CryAnimation/cry_1"], \
+        [SKTexture textureWithImageNamed:@"CryAnimation/cry_2"]  \
+    ]
+
+#define CHARACTER_ANIM_RUNANIMATION_RUN @[ \
+        [SKTexture textureWithImageNamed:@"RunAnimation/run_0"], \
+        [SKTexture textureWithImageNamed:@"RunAnimation/run_1"], \
+        [SKTexture textureWithImageNamed:@"RunAnimation/run_2"], \
+        [SKTexture textureWithImageNamed:@"RunAnimation/run_3"], \
+        [SKTexture textureWithImageNamed:@"RunAnimation/run_4"], \
+        [SKTexture textureWithImageNamed:@"RunAnimation/run_5"], \
+        [SKTexture textureWithImageNamed:@"RunAnimation/run_6"], \
+        [SKTexture textureWithImageNamed:@"RunAnimation/run_7"]  \
+    ]
+
+#define CHARACTER_ANIM_SHOOTANIMATION_SHOOT @[ \
+        [SKTexture textureWithImageNamed:@"ShootAnimation/shoot_0"], \
+        [SKTexture textureWithImageNamed:@"ShootAnimation/shoot_1"], \
+        [SKTexture textureWithImageNamed:@"ShootAnimation/shoot_2"], \
+        [SKTexture textureWithImageNamed:@"ShootAnimation/shoot_3"], \
+        [SKTexture textureWithImageNamed:@"ShootAnimation/shoot_4"], \
+        [SKTexture textureWithImageNamed:@"ShootAnimation/shoot_5"]  \
+    ]
+
+
+#endif // __CHARACTER_ATLAS__
